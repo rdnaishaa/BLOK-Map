5e5ce1b121aca2fc3187a3499d23fc51a05b14e5
DESKTOP-EV1O5OI\USER <raishasyauqi7@gmail.com>
2025-05-11 16:34:29 +0700

    duh reviewnya belum maaf gais

--- server/routes/review.route.js
+++ server/routes/review.route.js
@@ -5,14 +5,13 @@ const { verifyUser, verifyAdmin } = require('../middleware/auth');
 
 // User routes
 router.get('/:category/:id', ReviewController.getReviews);  
-router.post('/', verifyUser, ReviewController.createReview);  
+router.post('/', verifyUser, ReviewController.createReview);  // Pastikan ini mengarah ke fungsi yang benar
 router.put('/:id', verifyUser, ReviewController.updateReview);  
-router.delete('/:id', verifyUser, ReviewController.deleteReview);  
+router.delete('/:id', verifyUser, ReviewController.deleteReview);
 
 // Admin routes - Moderation
 router.delete('/admin/:id', verifyAdmin, ReviewController.deleteReview);
 router.put('/admin/:id/status', verifyAdmin, ReviewController.moderateReview); // approve/reject
 router.put('/admin/:id', verifyAdmin, ReviewController.adminEditReview); // edit review
-
 
 module.exports = router;
