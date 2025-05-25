8c030f59dcceb608f40cba5198fb7f933e403927
DESKTOP-EV1O5OI\USER <raishasyauqi7@gmail.com>
2025-05-25 20:51:46 +0700

    bismillah

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
   return (
     <div className="min-h-screen w-full flex flex-col bg-[#3D1E0F] text-white">
       <Header />
-      <main className="flex-grow container mx-auto px-4 sm:px-6 md:px-8 py-8">
+      <main className="flex-grow">
         {children}
       </main>
       <Footer />
