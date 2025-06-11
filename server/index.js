8a57366f243fb673ccf0a120166c0219f2ea387a
DESKTOP-EV1O5OI\USER <raishasyauqi7@gmail.com>
2025-06-11 16:54:16 +0700

    coba lagii

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://blok-map.vercel.app/', 'https://go-blok-m.vercel.app/'],
+  origin: ['http://localhost:5173', 'https://blok-map.vercel.app', 'https://go-blok-m.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
